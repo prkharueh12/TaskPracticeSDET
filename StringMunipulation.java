0e7c1c80d9fd3cd4e408f4a2e526a5c03b2ab84c
PARK <74276188+prkharueh12@users.noreply.github.com>
2021-01-01 11:29:09 -0500

    Update StringMunipulation.java

--- StringMunipulation.java
+++ StringMunipulation.java
@@ -5,6 +5,7 @@ public class StringMunipulation {
 	public static void main(String[] args) {
 		String name = "abcdef"; 
 		
+		//For loop
 		for (int i = name.length()-1; i >=0; i--) {
 			System.out.print(name.charAt(i)+ ", ");
 		}
